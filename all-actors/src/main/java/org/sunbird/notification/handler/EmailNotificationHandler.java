2f3386be509418fbef389061f2ea6f03816f16cb
shankaragoudab <140387294+shankaragoudab@users.noreply.github.com>
2025-09-04 12:18:05 +0530

    KB-11079 : notification changes : added modification for bcc flow (#3)
    * added bcc implementation to v2 notification
    
    * enhancement of notification service for the bcc flow

--- all-actors/src/main/java/org/sunbird/notification/handler/EmailNotificationHandler.java
+++ all-actors/src/main/java/org/sunbird/notification/handler/EmailNotificationHandler.java
@@ -33,7 +33,8 @@ public class EmailNotificationHandler implements INotificationHandler{
     public Response sendNotification(NotificationV2Request notificationRequest, boolean isDryRun, boolean isSync, Map<String,Object> reqContext) throws BaseException {
         logger.info("EmailNotificationHandler: making call to sendNotifications method");
         Response response = new Response();
-        if(null != notificationRequest && CollectionUtils.isNotEmpty(notificationRequest.getIds())){
+        if (null != notificationRequest && (CollectionUtils.isNotEmpty(notificationRequest.getIds()) ||
+                CollectionUtils.isNotEmpty(notificationRequest.getBccIds()))) {
             Map<String, Object> responseMap = new HashMap<String, Object>();
             Map<String,Object> template = Util.getTemplate(notificationRequest, notificationService, reqContext);
             Map<String,Object> dataTemplate =new HashMap<>();
@@ -56,6 +57,7 @@ public class EmailNotificationHandler implements INotificationHandler{
     private NotificationRequest createNotificationObj(NotificationV2Request notificationRequest, Map<String,Object> templateConfig) {
         NotificationRequest notification = new NotificationRequest();
         notification.setIds(notificationRequest.getIds());
+        notification.setBccIds(notificationRequest.getBccIds());
         notification.setMode(DeliveryMode.email.name());
         notification.setCopyEmail(notificationRequest.getCopyEmail());
         Config config = new Config();
